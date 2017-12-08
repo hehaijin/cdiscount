78149cc626647e019bea43089776344dd13fc0ca
hehaijin <hehaijin1981@gmail.com>
2017-12-07 21:45:18 -0700

    format code

--- train.py
+++ train.py
@@ -4,33 +4,37 @@ import cdiscountkeras
 from keras import optimizers
 from keras.utils import np_utils
 
+def main():
+	nb_epoch=1
 
-nb_epoch=1
 
+	bg=dataset.BatchGenerator(20)
+	X,Y=bg.__next__()
 
-bg=dataset.BatchGenerator(20)
-X,Y=bg.__next__()
+	datagen.fit(X)
+	my_model=cdiscountkeras.loadModel()
 
-datagen.fit(X)
-my_model=cdiscountkeras.loadModel()
+	sgd = optimizers.SGD(lr=0.0001)
+	my_model.compile(loss='categorical_crossentropy',
+				  optimizer=sgd,
+				  metrics=['accuracy'])
 
-sgd = optimizers.SGD(lr=0.0001)
-my_model.compile(loss='categorical_crossentropy',
-              optimizer=sgd,
-              metrics=['accuracy'])
-
-bg2=dataset.BatchGenerator(5000)              
-for e in range(nb_epoch):
-	print("big epoch %d" % e)
-	for X_train, Y_train in bg2:
-		Y_train= np_utils.to_categorical(Y_train,5271)
-		my_model.fit(X_train, Y_train,batch_size=32, epochs=1,verbose=1)
-     
- 
- 
-cdiscountkeras.saveModel(my_model)            
+	bg2=dataset.BatchGenerator(5000)              
+	for e in range(nb_epoch):
+		print("big epoch %d" % e)
+		for X_train, Y_train in bg2:
+			Y_train= np_utils.to_categorical(Y_train,5271)
+			my_model.fit(X_train, Y_train,batch_size=32, epochs=1,verbose=1)
+		 
+	 
+	 
+	cdiscountkeras.saveModel(my_model)            
               
-
+      
+      
+if __name__=='__main__':
+	main()
+ 
 
              
 
