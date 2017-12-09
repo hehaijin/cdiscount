e98015777a406a3e827ca3945cf44c817fbcdbca
hehaijin1981 <hehaijin1981@gmail.com>
2017-12-09 23:32:43 +0000

    put cluster image to folder

--- clusterConvertImage.py
+++ clusterConvertImage.py
@@ -5,18 +5,31 @@ import cv2
 def main():
 	savefolder='clusterfolder'
 	categories=['1000016400','1000016402','1000016404','1000021520','1000016406','1000016398','1000016408','1000021522','1000016416','1000016410','1000016412','1000016414']
-
-	bg=dataset.ClusterImageGenerator(5000)      
-
-	filecount=0
-	for pic,c in bg:
-		N=len(c):
-		for j in N:	
-			filecount=filecount+1
-			if filecount
-			try:
-				i=categories.index(c[j])
-				cv2.imwrite(path.join(savefolder, str(i)+"-"+count(filecount)+'.jpg') ,pic[j])
-			except ValueError:
-				pass	
 	
+	bg=dataset.ClusterImageGenerator(5000)      
+	
+	totalfilecount=0
+	fc=0
+	for pic,c in bg:
+		N=len(c)
+		print("batch length "+str(N))
+		for j in range(N):	
+			totalfilecount=totalfilecount+1
+			if totalfilecount%1000==0:
+				print(totalfilecount)
+			if c[j] in categories:
+				fc=fc+1
+				print("found file "+str(fc))
+				i=categories.index(c[j])
+				cv2.imwrite(path.join(savefolder, str(i)+"-"+str(fc)+'.jpg') ,pic[j])
+			
+	
+
+
+
+if __name__=="__main__":
+	main()
+
+
+
+
