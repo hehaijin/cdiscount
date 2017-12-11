c1f0c0feafd2e2c4cf454c7f1b65c90b89a7e3f8
hehaijin <hehaijin1981@gmail.com>
2017-12-10 20:55:36 -0700

    clearn up

--- clusterConvertImage.py
+++ clusterConvertImage.py
@@ -1,35 +1,22 @@
-#import dataset
+import dataset
 import os.path as path
-#import cv2
+import cv2
 
 def main():
 	savefolder='clusterfolder'
 	categories=['1000016400','1000016402','1000016404','1000021520','1000016406','1000016398','1000016408','1000021522','1000016416','1000016410','1000016412','1000016414']
-	print(len(categories))
+
 	bg=dataset.ClusterImageGenerator(5000)      
-	
-	totalfilecount=0
-	fc=0
+
+	filecount=0
 	for pic,c in bg:
-		N=len(c)
-		print("batch length "+str(N))
-		for j in range(N):	
-			totalfilecount=totalfilecount+1
-			if totalfilecount%1000==0:
-				print(totalfilecount)
-			if c[j] in categories:
-				fc=fc+1
-				print("found file "+str(fc))
+		N=len(c):
+		for j in N:	
+			filecount=filecount+1
+			if filecount
+			try:
 				i=categories.index(c[j])
-				cv2.imwrite(path.join(savefolder, str(i)+"-"+str(fc)+'.jpg') ,pic[j])
-			
+				cv2.imwrite(path.join(savefolder, str(i)+"-"+count(filecount)+'.jpg') ,pic[j])
+			except ValueError:
+				pass	
 	
-
-
-
-if __name__=="__main__":
-	main()
-
-
-
-
--- plot.py
+++ plot.py
@@ -2,12 +2,15 @@ import matplotlib.pyplot as plt
 
 
 
-accuracies=[0.45447,0.48142,0.51501,0.55375,0.58946,0.53646]
+accuracies=[0.45447,0.48142,0.51501,0.55375,0.58946]
 epochs=[]
-for i in range(6):
+for i in range(5):
 	epochs.append(i+1)
 
 plt.plot(epochs,accuracies)
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.grid(True)
 plt.show()
 
 
